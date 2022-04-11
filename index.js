a38a214c49cb995780b5972a3fe1c65a898f38b4
Antony Caseres <accasere@outlook.com>
2022-04-10 22:27:47 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers,string){
+    return drivers.filter(function(name){
+        return name.toUpperCase() === string.toUpperCase()
+    })
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => driver.slice(0,string.length)=== string)
+}
+
+function matchName (drivers, string){
+    return drivers.filter(driver => driver.name === string)
+}
